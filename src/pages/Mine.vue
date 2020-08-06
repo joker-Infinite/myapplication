55d37e692cab5135ae8a1f96ff6ed518d2b561e7
nieshihao <2075259561@qq.com>
2020-08-06 17:00:58 +0800

    MayApplication-退出登录功能

--- src/pages/Mine.vue
+++ src/pages/Mine.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container">
-        <vue-header></vue-header>
-        <div style="width: 100%;height: 40px"></div>
+        <div class="userInfo">
+            <el-button @click="dropOut">退出登录</el-button>
+        </div>
         <vue-footer></vue-footer>
     </div>
 </template>
@@ -9,18 +10,47 @@
 <script>
     import VueHeader from "../components/vueHeader";
     import VueFooter from "../components/vueFooter";
+    import store from "../store";
 
     export default {
         name: "Mine",
-        components: {VueFooter, VueHeader}
+        components: {VueFooter, VueHeader},
+        data() {
+            return {}
+        },
+        methods: {
+            dropOut() {
+                let time = new Date();
+                let v = document.cookie;
+                time.setTime(time.getTime() + 24 * 60 * 60 * 1000);//过期时间1天
+                let userData = JSON.parse(v.split('=')[1]);
+                userData.forEach(i => {
+                    i.status = false;
+                });
+                document.cookie = 'myApplication=' + JSON.stringify(userData) + ";expires=" + time.toGMTString();
+                this.$store.commit('actionIncrease', {
+                    userInfo: {
+                        userAccountNumber: '',
+                        userPassword: ''
+                    },
+                    status: false,
+                    recording: {}
+                });
+                this.$router.push('/login');
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style scoped lang="less">
     .container {
         position: fixed;
         width: 100%;
         height: 100%;
         background: #F5F7FA;
+
+        .userInfo {
+
+        }
     }
 </style>
